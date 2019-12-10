ddd407eba87b783bae751012647344483ed8f27c
Luis Brime <brimegmz@gmail.com>
2019-12-09 23:20:07 -0600

    Another one :shipit:2

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
 
     solver = QueensSolver(8)
     for solution in solver.solutions:
-        query = db.instert(solutions_table).values(solution_string = str(solution))
+        query = db.insert(solutions_table).values(solution_string = str(solution))
         ResultProxy = connection.execute(query)
