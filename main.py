1e16da7236e024bfefb967b935f0b7510e04be00
Luis Brime <brimegmz@gmail.com>
2019-12-10 23:22:07 -0600

    Into the unknown

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ if __name__ == "__main__":
     engine = db.create_engine('postgresql+psycopg2://brime:panda@db/queensdb')
     connection = engine.connect()
     metadata = db.MetaData()
-    solutions_table = db.Table('solutions', metadata, db.Column('solution_string', db.String(25), nullable=False))
+    solutions_table = db.Table('solutions', metadata, db.Column('solution_id', db.Integer(), db.Sequence('id'), primary_key=True) db.Column('solution_string', db.String(25), nullable=False))
+
+    metadata.create_all(engine)
+
     payload = []
 
     solver = QueensSolver(8)
